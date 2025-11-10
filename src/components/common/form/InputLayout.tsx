785eafe8dffa0a1f512c63e0a4982358cefe8c43
Gema Dodi Pranata <gemadp01@gmail.com>
2025-11-10 16:09:47 +0700

    WIP: input component, auth page layout

---
+++ src/components/common/form/InputLayout.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+type TInputLayout = {
+  label: string;
+  inputRequired?: boolean;
+  type: string;
+  placeholder?: string;
+  inputIconSupport?: string;
+  helperMessage?: string;
+  helperIconSupport?: string;
+  errorMessage?: string;
+  disabled?: boolean;
+} & React.ComponentPropsWithoutRef<"input">;
+
+const InputLayout = ({
+  label = "label",
+  inputRequired = false,
+  type = "text",
+  placeholder,
+  // inputIconSupport,
+  helperMessage,
+  // helperIconSupport,
+  errorMessage,
+  disabled = false,
+}: TInputLayout) => {
+  // const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <>
+      <div className="mb-4">
+        <label className="block mb-2 text-s-regular text-neutral-90">
+          {label}
+          {inputRequired && <span className="text-danger-main">*</span>}
+        </label>
+
+        <div
+          // className={`relative flex items-center mb-2 border-2 border-neutral-40 rounded-lg bg-white ${
+          //   isFocused ? "border-primary-main" : "text-neutral-90"
+          // } ${disabled ? "bg-neutral-30" : ""}`}
+          className="relative flex items-center mb-2 border-2 border-neutral-40 rounded-lg bg-neutral-10"
+        >
+          {/* <div className="flex items-center justify-center pl-4 pr-2 py-3">
+          {inputIconSupport}
+        </div> */}
+
+          <input
+            type={type}
+            // onFocus={() => setIsFocused(true)}
+            // onBlur={() => setIsFocused(false)}
+            placeholder={placeholder}
+            // className={`w-full py-2 bg-transparent focus:outline-none placeholder-neutral-60 ${
+            //   disabled && "cursor-not-allowed"
+            // }`}
+            className="w-full py-2 px-4 bg-transparent focus:outline-none placeholder-neutral-60"
+            disabled={disabled}
+          />
+        </div>
+
+        {/* Helper Message and Counter */}
+        <div
+          className={`flex items-center justify-between mt-2 text-s-regular ${
+            errorMessage ? "text-danger-main" : "text-neutral-70"
+          }`}
+        >
+          <span className="flex items-center">
+            {/* <div className="w-5 h-5 rounded-full border-2 border-gray-400 mr-2"></div> */}
+            {helperMessage}
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default InputLayout;
---
+++ src/components/common/layout/AuthPageLayout.tsx
@@ -0,0 +1,68 @@
+import { Link, useLocation } from "react-router";
+
+type TAuthPageLayout = {
+  headTitle: string;
+  headDesc: string;
+  headLink: string;
+  children: React.ReactNode;
+  footButton: React.ReactNode;
+};
+
+function AuthPageLayout({
+  headTitle,
+  headDesc,
+  headLink,
+  children,
+  footButton,
+}: TAuthPageLayout) {
+  const location = useLocation();
+
+  return (
+    <div className="min-h-screen bg-[rgba(250,250,250,1)] flex items-center justify-center">
+      <div className="w-full max-w-[500px]">
+        {/* Logo */}
+
+        <div className="mb-4">
+          <img
+            src="/public/Logo Rakamin.png"
+            alt="Rakamin Logo Type"
+            className="w-[145px] "
+          />
+        </div>
+
+        {/* Card */}
+        <div className="bg-neutral-10 shadow-md p-10">
+          <h2 className="text-heading-s-bold text-neutral-90 mb-2">
+            {headTitle}
+          </h2>
+
+          <p className="text-m-regular mb-4">
+            {headDesc}{" "}
+            <Link
+              to={
+                location.pathname.startsWith("/login") ? "/login" : "/register"
+              }
+              className="text-primary-main"
+            >
+              {headLink}
+            </Link>
+          </p>
+
+          {children}
+
+          {/* Divider */}
+          <div className="flex items-center my-4">
+            <div className="flex-1 border-t border-neutral-60"></div>
+            <span className="px-4 text-sm text-neutral-60">or</span>
+            <div className="flex-1 border-t border-neutral-60"></div>
+          </div>
+
+          {/* Google Button */}
+          {footButton}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AuthPageLayout;
