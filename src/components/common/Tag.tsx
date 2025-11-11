199d18eb6173c1677612377bc80c7e4306ff0fa6
Gema Dodi Pranata <gemadp01@gmail.com>
2025-11-11 08:24:15 +0700

    feat: add tag component

---
+++ src/components/common/Tag.tsx
@@ -0,0 +1,9 @@
+export const Tag = () => {
+  return (
+    <div className="flex justify-around items-center">
+      <span className="text-s-bold text-success-main px-2 bg-success-surface border border-success-border rounded-sm">
+        Tags
+      </span>
+    </div>
+  );
+};
