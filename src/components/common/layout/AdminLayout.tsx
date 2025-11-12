dc1c6b26a70a60a5d83acce410a4fa98aa53a17e
Gema Dodi Pranata <gemadp01@gmail.com>
2025-11-12 16:49:13 +0700

    refactor(admin-layout): adjust header style and props structure

--- src/components/common/layout/AdminLayout.tsx
+++ src/components/common/layout/AdminLayout.tsx
@@ -1,7 +1,7 @@
 import { UserIcon } from "@heroicons/react/16/solid";
 
 type TAdminLayoutProps = {
-  headTitle: string | React.ReactNode;
+  headTitle: React.ReactNode;
   children: React.ReactNode;
 };
 
@@ -9,15 +9,17 @@ function AdminLayout({ headTitle, children }: TAdminLayoutProps) {
   return (
     <div className="min-h-screen">
       {/* Header */}
-      <header className="bg-neutral-10 border-b border-neutral-40 px-6 py-4 flex items-center justify-between">
-        <h1 className="text-l-bold">{headTitle}</h1>
-        <button className="w-7 h-7 rounded-full  flex items-center justify-center border border-neutral-40">
-          <UserIcon className="w-5 h-5 text-neutral-90" />
-        </button>
+      <header className="bg-neutral-10 border-b border-neutral-40 px-6 py-4">
+        <div className="max-w-7xl mx-auto flex justify-between items-center">
+          {headTitle}
+          <button className="w-7 h-7 rounded-full flex items-center justify-center border border-neutral-40">
+            <UserIcon className="w-5 h-5 text-neutral-90" />
+          </button>
+        </div>
       </header>
 
       {/* Main Content */}
-      <main className="mx-auto px-6 py-9">{children}</main>
+      <main className="max-w-7xl mx-auto px-6 py-8">{children}</main>
     </div>
   );
 }
