ee291a026dfff436a77debde9583c97161709a62
Gema Dodi Pranata <gemadp01@gmail.com>
2025-11-12 16:47:06 +0700

    refactor(tag): simplify component structure and improve readability

--- src/components/common/Tag.tsx
+++ src/components/common/Tag.tsx
@@ -1,9 +1,39 @@
-export const Tag = () => {
-  return (
-    <div className="flex justify-around items-center">
-      <span className="text-s-bold text-success-main px-2 bg-success-surface border border-success-border rounded-sm">
-        Tags
-      </span>
-    </div>
-  );
+import { cva } from "class-variance-authority";
+import clsx from "clsx";
+
+type TTag = {
+  children: React.ReactNode;
+  variant?:
+    | "outlineSuccess"
+    | "filledSuccess"
+    | "outlineDanger"
+    | "filledDanger"
+    | "outlineSecondary"
+    | "filledSecondary";
+  size?: "sm" | "md";
+};
+
+const tag = cva("text-center py-1 px-2 rounded-sm", {
+  variants: {
+    variant: {
+      outlineSuccess: "text-success-main border border-success-border",
+      filledSuccess: "text-neutral-10 bg-success-main ",
+      outlineDanger: "text-danger-main border border-danger-border",
+      filledDanger: "text-neutral-10 bg-danger-main",
+      outlineSecondary: "text-secondary-main border border-secondary-border",
+      filledSecondary: "text-neutral-10 bg-secondary-main",
+    },
+    size: {
+      sm: "text-s-bold",
+      md: "text-m-bold",
+    },
+  },
+  defaultVariants: {
+    variant: "outlineSuccess",
+    size: "md",
+  },
+});
+
+export const Tag = ({ children, variant, size }: TTag) => {
+  return <span className={clsx(tag({ variant, size }))}>{children}</span>;
 };
