3d833839f53b1c7bfa3eed3c49645ed18a5660ef
Gema Dodi Pranata <gemadp01@gmail.com>
2025-11-13 08:09:36 +0700

    refactor: restructure base modal component

--- src/components/common/modals/ModalBase.tsx
+++ src/components/common/modals/ModalBase.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 type TModalBaseProps = {
   headTitle: string;
   isOpen: boolean;
@@ -6,32 +8,34 @@ type TModalBaseProps = {
   children: React.ReactNode;
 };
 
-export function ModalBase({
-  headTitle,
-  isOpen,
-  handleCloseModal,
-  icon: Icon,
-  children,
-}: TModalBaseProps) {
-  if (!isOpen) return null;
+export const ModalBase = React.memo(
+  ({
+    headTitle,
+    isOpen,
+    handleCloseModal,
+    icon: Icon,
+    children,
+  }: TModalBaseProps) => {
+    if (!isOpen) return null;
 
-  return (
-    <div className="fixed inset-0 bg-neutral-100/50 flex items-center justify-center z-50 p-4">
-      <div className="bg-[rgba(255,255,255,1)] rounded-lg w-full max-w-4xl max-h-[90vh] overflow-hidden flex flex-col">
-        {/* Modal Header */}
-        <div className="flex items-center justify-between p-6 border-b border-neutral-40">
-          <h2 className="text-l-bold text-neutral-100">{headTitle}</h2>
-          <button
-            onClick={handleCloseModal}
-            className="text-gray-500 hover:text-gray-700 transition-colors"
-          >
-            <Icon className="w-5 h-5 text-neutral-90" />
-          </button>
+    return (
+      <div className="fixed inset-0 bg-neutral-100/50 flex items-center justify-center z-50 p-4">
+        <div className="bg-[rgba(255,255,255,1)] rounded-lg w-full max-w-4xl max-h-[90vh] overflow-hidden flex flex-col">
+          {/* Modal Header */}
+          <div className="flex items-center justify-between p-6 border-b border-neutral-40">
+            <h2 className="text-l-bold text-neutral-100">{headTitle}</h2>
+            <button
+              onClick={handleCloseModal}
+              className="text-gray-500 hover:text-gray-700 transition-colors"
+            >
+              <Icon className="w-5 h-5 text-neutral-90" />
+            </button>
+          </div>
+
+          {/* Modal Body */}
+          <div className="flex-1 overflow-y-auto p-6">{children}</div>
         </div>
-
-        {/* Modal Body */}
-        <div className="flex-1 overflow-y-auto p-6">{children}</div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
