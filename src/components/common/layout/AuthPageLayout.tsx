539ea3b9d90dbcd9db9e2196934f782a80f6892f
Gema Dodi Pranata <gemadp01@gmail.com>
2025-11-16 23:40:36 +0700

    feat(auth): implement login logic

--- src/components/common/layout/AuthPageLayout.tsx
+++ src/components/common/layout/AuthPageLayout.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import { Link, useLocation } from "react-router";
 
 type TAuthPageLayout = {
@@ -8,61 +9,65 @@ type TAuthPageLayout = {
   footButton: React.ReactNode;
 };
 
-function AuthPageLayout({
-  headTitle,
-  headDesc,
-  headLink,
-  children,
-  footButton,
-}: TAuthPageLayout) {
-  const location = useLocation();
+const AuthPageLayout = memo(
+  ({
+    headTitle,
+    headDesc,
+    headLink,
+    children,
+    footButton,
+  }: TAuthPageLayout) => {
+    const location = useLocation();
 
-  return (
-    <div className="min-h-screen bg-[rgba(250,250,250,1)] flex items-center justify-center">
-      <div className="w-full max-w-[500px]">
-        {/* Logo */}
+    return (
+      <div className="min-h-screen bg-[rgba(250,250,250,1)] flex items-center justify-center">
+        <div className="w-full max-w-[500px]">
+          {/* Logo */}
 
-        <div className="mb-4">
-          <img
-            src="/public/Logo Rakamin.png"
-            alt="Rakamin Logo Type"
-            className="w-[145px] "
-          />
-        </div>
-
-        {/* Card */}
-        <div className="bg-neutral-10 shadow-md p-10">
-          <h2 className="text-heading-s-bold text-neutral-90 mb-2">
-            {headTitle}
-          </h2>
-
-          <p className="text-m-regular mb-4">
-            {headDesc}{" "}
-            <Link
-              to={
-                location.pathname.startsWith("/login") ? "/register" : "/login"
-              }
-              className="text-primary-main"
-            >
-              {headLink}
-            </Link>
-          </p>
-
-          {children}
-
-          {/* Divider */}
-          <div className="flex items-center my-4">
-            <div className="flex-1 border-t border-neutral-60"></div>
-            <span className="px-4 text-sm text-neutral-60">or</span>
-            <div className="flex-1 border-t border-neutral-60"></div>
+          <div className="mb-4">
+            <img
+              src="/public/Logo Rakamin.png"
+              alt="Rakamin Logo Type"
+              className="w-[145px] "
+            />
           </div>
 
-          {/* Google Button */}
-          {footButton}
+          {/* Card */}
+          <div className="bg-neutral-10 shadow-md p-10">
+            <h2 className="text-heading-s-bold text-neutral-90 mb-2">
+              {headTitle}
+            </h2>
+
+            <p className="text-m-regular mb-1">
+              {headDesc}{" "}
+              <Link
+                to={
+                  location.pathname.startsWith("/login")
+                    ? "/register"
+                    : "/login"
+                }
+                className="text-primary-main"
+              >
+                {headLink}
+              </Link>
+            </p>
+
+            {children}
+
+            {/* Divider */}
+            <div className="flex items-center my-4">
+              <div className="flex-1 border-t border-neutral-60"></div>
+              <span className="px-4 text-sm text-neutral-60">or</span>
+              <div className="flex-1 border-t border-neutral-60"></div>
+            </div>
+
+            {/* Google Button */}
+            {footButton}
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
 
 export default AuthPageLayout;
